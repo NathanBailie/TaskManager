e7f1d72b652ac514bcf0b6265c1738e3f47c6941
Nathan <nathanbailie.dev@gmail.com>
2023-06-18 16:38:44 +0300

    made the main windowm, variables, one mixin and button styles

--- src/components/MainWindow/MainWindow.vue
+++ src/components/MainWindow/MainWindow.vue
@@ -7,6 +7,24 @@ export default {
 </script>
 <template>
   <div class="mainWindow">
-    <p>Main</p>
+    <h1>Task Manager</h1>
+    <div class="mainWindow__taskWraper red">
+      <div class="button red">
+        <a href="/high">High Priority Tasks</a>
+      </div>
+      <span>0</span>
+    </div>
+    <div class="mainWindow__taskWraper orange">
+      <div class="button orange">
+        <a href="/middle">Middle Priority Tasks</a>
+      </div>
+      <span>0</span>
+    </div>
+    <div class="mainWindow__taskWraper green">
+      <div class="button green">
+        <a href="/low">Low Priority Tasks</a>
+      </div>
+      <span>0</span>
+    </div>
   </div>
 </template>
